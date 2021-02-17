bf6af6441cd7a186307e453a299efb3175f95e74
noam.buc <noambbc@gmail.com>
2021-02-17 14:24:52 +0200

    created highest number function and test

--- src/main.py
+++ src/main.py
@@ -1,2 +1,16 @@
 def addition(x, y):
     return x + y
+
+
+def difference(x, y):
+    if x > y:
+        return x - y
+    if y > x:
+        return y - x
+
+
+def highest_number(x, y):
+    if x > y:
+        return x
+    else:
+        return y
